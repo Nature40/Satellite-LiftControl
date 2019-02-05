a5918ccd36da831cdfe432bfbbde2ae3af4075d4
Jonas Höchst <git@jonashoechst.de>
2019-02-05 15:20:47 +0100

    motor control via ESP implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <WiFi.h>
 
-const char *ssid = "foobar";
+// connections to L298N
+#define EN_A 21
+#define IN1 13
+#define IN2 12
+
+// PWM motor configuration
+const int freq = 255;
+const int chan = 0;
+const int resolution = 8;
+
+// Network configuration
+char ssid [30];
 const char *pass = "supersicher";
-const int   port = 35037;
+const int port = 35037;
 
 WiFiServer server;
 WiFiUDP udp;
@@ -13,17 +24,38 @@ void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
 
+  uint64_t chipid = ESP.getEfuseMac();
+  Serial.printf("ESP32 Chip ID: %04x\n", (uint16_t)(chipid>>32));
+
+  snprintf(ssid, 30, "LiftSystem %04x", (uint16_t)(chipid>>32));
   WiFi.softAP(ssid, pass);
   server.begin();
 
-  Serial.print("GuMo ");
-  Serial.print(WiFi.softAPIP());
-  Serial.print("\n");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  Serial.print("Password: ");
+  Serial.println(pass);
+  Serial.print("IP: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.print("Port: ");
+  Serial.println(port);
 
   if (!udp.begin(WiFi.softAPIP(), port)) {
-    Serial.println("Failed to start UDP server");
+    Serial.println("Error: Failed to start UDP server");
     while (1);
   }
+
+  // setup in pins
+  pinMode(EN_A, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+
+  // setup PWM channel
+  ledcSetup(chan, freq, resolution);
+  ledcAttachPin(EN_A, chan);
+
 }
 
 void loop() {
@@ -33,7 +65,12 @@ void loop() {
   }
 
   udp.read(buff, len);
-  int cmdData = atoi(buff);
+  int cmd_data = atoi(buff);
 
-  Serial.println(cmdData);
+  Serial.println(cmd_data);
+
+  ledcWrite(chan, abs(cmd_data));
+
+  digitalWrite(IN1, (cmd_data < 0));
+  digitalWrite(IN2, (cmd_data > 0));
 }
