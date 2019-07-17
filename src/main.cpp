ed150f91d11cfe39481d405fa4213d7b39bfc54a
Jonas Höchst <git@jonashoechst.de>
2019-07-17 10:49:23 +0200

    working BLE variant

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-
+#include <BLE2902.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
 #include <SSD1306.h>
 #include <WiFi.h>
-#include <esp_wifi.h>
 
 // connections to L298N
 #define EN_A 21
@@ -10,36 +12,21 @@
 
 #define BUTTON_DOWN 32
 #define BUTTON_UP 33
+enum direction_t {
+    down = -1,
+    stop = 0,
+    up = 1,
+};
 
-// motor timeout (security fallback)
-int timeout_ms = 500;
+// motor runtime properties
+uint16_t timeout_ms = 500;
+uint8_t speed = 0;
+direction_t direction = stop;
 
 // PWM motor configuration
 const int freq = 255;
 const int chan = 0;
 const int resolution = 8;
-
-// Network configuration
-char ssid[30];
-const char *pass = "supersicher";
-const int port = 35037;
-const IPAddress ip = IPAddress(192, 168, 3, 254);
-const IPAddress gateway = IPAddress(192, 168, 3, 254);
-const IPAddress subnet = IPAddress(255, 255, 255, 0);
-
-WiFiServer server;
-WiFiUDP udp;
-
-#define MAX_UDP_SIZE 65536
-char buffer[MAX_UDP_SIZE];
-
-// timeout for motor command
-int timeout = 0;
-
-// parameters of last client
-IPAddress remoteIP(0, 0, 0, 0);
-uint16_t remotePort = 0;
-
 // display configuration
 #define OLED_ADDRESS 0x3c
 #define OLED_SDA 4
@@ -49,139 +36,43 @@ SSD1306 display(OLED_ADDRESS, OLED_SDA, OLED_SCL);
 
 int display_speed = 0;
 int display_redraw = 0;
+unsigned long timeout_ts = 0;
 
-void sendResponse(int payload_len) {
-    udp.beginPacket(remoteIP, remotePort);
-    udp.write((const uint8_t *)buffer, payload_len);
-    udp.endPacket();
-}
+// instance values
+char name[30];
 
-void setTimeout(int new_timeout_ms) {
-    Serial.printf("Setting timeout_ms to %i\n", new_timeout_ms);
-    timeout_ms = new_timeout_ms;
+#define BLE_SERVICE_LIFT_UUID BLEUUID((uint16_t)35037)
+#define BLE_CHAR_TIMEOUT_UUID BLEUUID((uint16_t)0xFF00)
+#define BLE_CHAR_DIRECTION_UUID BLEUUID((uint16_t)0xFF01)
+#define BLE_CHAR_SPEED_UUID BLEUUID((uint16_t)0xFF02)
 
-    int payload_len =
-        snprintf(buffer, MAX_UDP_SIZE, "timeout %i\n", new_timeout_ms);
-    sendResponse(payload_len);
-}
-
-void setSpeed(int speed, int timeout_ms) {
-    // cutoff extreme values
-    if (speed > 255)
-        speed = 255;
-    if (speed < -255)
-        speed = -255;
-
-    Serial.printf("Setting speed to %i\n", speed);
-    display_speed = speed;
-    timeout = millis() + timeout_ms;
-
-    // write 255 if standing still or pwm value
-    if (speed == 0)
-        ledcWrite(chan, 255);
-    else
-        ledcWrite(chan, abs(speed));
-
-    // write direction (double zero to break)
-    digitalWrite(IN1, (speed < 0));
-    digitalWrite(IN2, (speed > 0));
-
-    // send packet to last controller
-    int payload_len = snprintf(buffer, MAX_UDP_SIZE, "set %i\n", speed);
-    sendResponse(payload_len);
-}
-
-void parsePacket() {
-    // separate cmd string
-    char *cmd = strtok(buffer, " ");
-    if (cmd == NULL) {
-        Serial.printf("Error: empty command\n");
-        return;
-    }
-
-    char *arg1 = strtok(NULL, " ");
-
-    if (!strcmp(cmd, "speed")) {
-        if (arg1 == NULL) {
-            Serial.printf("Error: argument <speed> missing\n");
-        } else {
-            int speed = atoi(arg1);
-            setSpeed(speed, timeout_ms);
-        }
-    } else if (!strcmp(cmd, "timeout")) {
-        if (arg1 == NULL) {
-            Serial.printf("Error: argument <ms> missing\n");
-        } else {
-            int new_timeout_ms = atoi(arg1);
-            setTimeout(new_timeout_ms);
-        }
-    } else {
-        Serial.printf("Command '%s' is unknown, skipping\n", cmd);
-    }
-}
-
-bool handlePacket() {
-    int packetSize = udp.parsePacket();
-
-    // sometimes udp.parsePacket() gets stuck in a loop, a reboot helps here.
-    if (errno == 5)
-        ESP.restart();
-
-    if (packetSize) {
-        remoteIP = udp.remoteIP();
-        remotePort = udp.remotePort();
-
-        size_t payload_len = udp.read(buffer, MAX_UDP_SIZE);
-        buffer[payload_len] = 0;
-
-        Serial.printf("Received %i bytes from %s:%i: '%s'\n", payload_len,
-                      remoteIP.toString().c_str(), remotePort, buffer);
-
-        parsePacket();
-    }
-
-    return (packetSize > 0);
-}
-
-bool handleButtons() {
-    if (digitalRead(BUTTON_UP) && digitalRead(BUTTON_DOWN)) {
-        Serial.println("Both buttons pressed, stopping lift.");
-        setSpeed(0, 0);
-        return true;
-    }
-
-    if (digitalRead(BUTTON_UP)) {
-        Serial.println("Button up pressed.");
-        setSpeed(255, 0);
-        return true;
-    }
-
-    if (digitalRead(BUTTON_DOWN)) {
-        Serial.println("Button down pressed.");
-        setSpeed(-255, 0);
-        return true;
-    }
-
-    return false;
-}
+BLECharacteristic timeoutChar(BLE_CHAR_TIMEOUT_UUID,
+                              BLECharacteristic::PROPERTY_READ |
+                                  BLECharacteristic::PROPERTY_WRITE |
+                                  BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic directionChar(BLE_CHAR_DIRECTION_UUID,
+                                BLECharacteristic::PROPERTY_READ |
+                                    BLECharacteristic::PROPERTY_WRITE |
+                                    BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic speedChar(BLE_CHAR_SPEED_UUID,
+                            BLECharacteristic::PROPERTY_READ |
+                                BLECharacteristic::PROPERTY_WRITE |
+                                BLECharacteristic::PROPERTY_NOTIFY);
 
 void redraw() {
-    // get station count
-    wifi_sta_list_t stations;
-    esp_wifi_ap_get_sta_list(&stations);
     char stations_str[16];
     char speed_str[16];
     char timeout_str[16];
-    snprintf(stations_str, 16, "%i", stations.num);
+    snprintf(stations_str, 16, "%i", 0);
     snprintf(speed_str, 16, "%i", display_speed);
-    double timeout_s = ((double)millis() - (double)timeout) / 1000;
-    snprintf(timeout_str, 16, "%.1f", timeout_s);
+    double timeout_s = ((double)millis() - (double)timeout_ts) / 1000;
+    snprintf(timeout_str, 16, "%15.1f", timeout_s);
 
     display.clear();
     display.flipScreenVertically();
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(ArialMT_Plain_24);
-    display.drawString(0, 0, ssid + 20);
+    display.drawString(0, 0, name + 20);
 
     display.setFont(ArialMT_Plain_10);
     display.drawString(0, 24, "WiFi Stations");
@@ -197,6 +88,119 @@ void redraw() {
     display_redraw = millis() + 100;
 }
 
+void setSpeed(direction_t _direction, uint8_t _speed) {
+    Serial.printf("INFO - Setting speed to %u, direction %i\n", _speed,
+                  _direction);
+    timeout_ts = millis() + timeout_ms;
+
+    ledcWrite(chan, abs(speed));
+
+    digitalWrite(IN1, (_direction == down));
+    digitalWrite(IN2, (_direction == up));
+
+    speed = _speed;
+    direction = _direction;
+
+    speedChar.setValue(&speed, 1);
+    speedChar.notify();
+    directionChar.setValue((uint8_t *)&direction, 1);
+    directionChar.notify();
+
+    redraw();
+}
+
+class TimeoutCharCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *timeoutChar) {
+        int len = timeoutChar->getValue().length();
+
+        if (len != 2) {
+            Serial.printf("ERROR - received %i bytes for timeout, ignoring.\n ",
+                          len);
+            timeoutChar->setValue((uint8_t *)&timeout_ms, 2);
+            return;
+        }
+
+        uint16_t _timeout_ms = *((uint16_t *)timeoutChar->getData());
+        Serial.printf("INFO - BLE received timeout (%u byte): %u\n", len,
+                      _timeout_ms);
+
+        timeout_ms = _timeout_ms;
+    }
+};
+
+class SpeedCharCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *speedChar) {
+        int len = speedChar->getValue().length();
+
+        if (len != 1) {
+            Serial.printf("ERROR - received %u bytes for speed, ignoring.\n",
+                          len);
+            speedChar->setValue(&speed, 1);
+            return;
+        }
+
+        uint8_t _speed = *speedChar->getData();
+        Serial.printf("INFO - BLE received speed (%u byte): %u\n", len, _speed);
+
+        setSpeed(direction, _speed);
+    }
+};
+
+class DirectionCharCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *directionChar) {
+        int len = directionChar->getValue().length();
+
+        if (len != 1) {
+            Serial.printf(
+                "ERROR - received %i bytes for direction, ignoring.\n ", len);
+            directionChar->setValue((uint8_t *)&direction, 1);
+            return;
+        }
+
+        int8_t _direction_recv = (int8_t)*directionChar->getData();
+        direction_t _direction = (direction_t)_direction_recv;
+
+        // Test if received value is acceptable
+        switch (_direction) {
+        case down:
+        case stop:
+        case up:
+            break;
+        default:
+            Serial.printf("ERROR - received direction %i, ignoring.\n ",
+                          _direction);
+            directionChar->setValue((uint8_t *)&direction, 1);
+            return;
+        }
+
+        Serial.printf("INFO - BLE received direction (%u byte): %i\n", len,
+                      _direction);
+        setSpeed(_direction, speed);
+    }
+};
+
+bool handleButtons() {
+    if (digitalRead(BUTTON_UP) && digitalRead(BUTTON_DOWN)) {
+        Serial.println("INFO - Both buttons pressed, stopping lift.");
+        setSpeed(stop, 0);
+        return true;
+    }
+
+    if (digitalRead(BUTTON_UP)) {
+        Serial.println("INFO - Button up pressed.");
+        setSpeed(up, 255);
+        return true;
+    }
+
+    if (digitalRead(BUTTON_DOWN)) {
+        Serial.println("INFO - Button down pressed.");
+        setSpeed(down, 255);
+        return true;
+    }
+
+    return false;
+}
+
 void setup() {
     // setup serial console output
     Serial.begin(115200);
@@ -204,8 +208,8 @@ void setup() {
 
     // read chip id
     uint64_t chipid = ESP.getEfuseMac();
-    Serial.printf("ESP32 Chip ID: %04x\n", (uint16_t)(chipid >> 32));
-    snprintf(ssid, 30, "nature40-liftsystem-%04x", (uint16_t)(chipid >> 32));
+    Serial.printf("INFO: ESP32 Chip ID: %04x\n", (uint16_t)(chipid >> 32));
+    snprintf(name, 30, "nature40-liftsystem-%04x", (uint16_t)(chipid >> 32));
 
     // Reset OLED
     pinMode(OLED_RST, OUTPUT);
@@ -216,26 +220,10 @@ void setup() {
     // Init OLED
     display.init();
     redraw();
-    Serial.println("OLED display initialised.");
-
-    // setup WiFi access point
-    WiFi.softAP(ssid, pass);
-    WiFi.softAPConfig(ip, gateway, subnet);
-    server.begin();
-
-    // start udp server
-    if (!udp.begin(WiFi.softAPIP(), port)) {
-        Serial.println("Error: Failed to start UDP server");
-        while (true) {
-            delay(1000);
-        }
-    }
+    Serial.println("INFO - OLED display initialised.");
 
     // print configuration
-    Serial.printf("SSID: %s\n", ssid);
-    Serial.printf("Password: %s\n", pass);
-    Serial.printf("IP: %s\n", WiFi.softAPIP().toString().c_str());
-    Serial.printf("Port: %i\n", port);
+    Serial.printf("INFO - Name: %s\n", name);
 
     // setup manual control buttons
     pinMode(BUTTON_DOWN, INPUT);
@@ -250,31 +238,44 @@ void setup() {
     ledcSetup(chan, freq, resolution);
     ledcAttachPin(EN_A, chan);
 
-    // set initial break
-    setSpeed(0, timeout_ms);
+    // set callback methods
+    timeoutChar.setCallbacks(new TimeoutCharCallbacks());
+    directionChar.setCallbacks(new DirectionCharCallbacks());
+    speedChar.setCallbacks(new SpeedCharCallbacks());
+
+    // setup BLE server, service and characteristics
+    BLEDevice::init(name);
+    BLEServer *bleServer = BLEDevice::createServer();
+    BLEService *liftService = bleServer->createService(BLE_SERVICE_LIFT_UUID);
+    liftService->addCharacteristic(&timeoutChar);
+    liftService->addCharacteristic(&speedChar);
+    liftService->addCharacteristic(&directionChar);
+    bleServer->getAdvertising()->addServiceUUID(BLE_SERVICE_LIFT_UUID);
+
+    // start lift service
+    liftService->start();
+    bleServer->getAdvertising()->start();
+
+    // announce timeout and set initial break
+    timeoutChar.setValue((uint8_t *)&timeout_ms, 2);
+    timeoutChar.notify();
+    setSpeed(down, 0);
 }
 
 void loop() {
     bool button_pressed = handleButtons();
-    bool packet_received = false;
-
-    if (!button_pressed) {
-        packet_received = handlePacket();
-    }
-
-    // if a timeout occures and the lift moves
-    if ((timeout < millis()) && (digitalRead(IN1) || digitalRead(IN2))) {
-        setSpeed(0, 0);
-    }
 
     if (display_redraw < millis()) {
         redraw();
     }
 
-    // if a packet is received, check for the next, else sleep.
-    if (packet_received) {
-        return;
-    } else {
-        delay(10);
+    if (timeout_ts < millis()) {
+        if (direction != stop) {
+            Serial.printf("INFO - Hit timeout, stopping lift");
+            setSpeed(stop, speed);
+            timeout_ts = millis();
+        }
     }
+
+    delay(10);
 }
